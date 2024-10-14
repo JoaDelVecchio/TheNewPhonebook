a95a0ab6b6d8ef3c4cc2d98a281b8674f7407faf
Joaquin Del Vecchio <172492638+JoaDelVecchio@users.noreply.github.com>
2024-10-14 15:28:15 -0300

    .

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,7 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
-export default {
-  // Other Vite config options...
-  build: {
-    rollupOptions: {
-      external: ["axios"],
-    },
-  },
-};
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react()],
+});
